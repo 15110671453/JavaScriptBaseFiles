5a64928584e853f81bfa8e57220679b48da70d49
15110671453 <15110671453@163.com>
2016-10-26 14:40:13 +0800

    commit 购物车完成
    commit 购物车完成

--- base.js
+++ base.js
@@ -73,8 +73,8 @@ func2();  // 报错  TypeError: number is not a function
     base.productsJiuPinData = [];
         }else{
             this.productsDianZiData = productManager.productData[0].products;
-            this.productsShiPInData = productManager.productData[1].products;
-            this.productsRiYongPinData = productManager.productData[2].products;
+            this.productsRiYongPinData = productManager.productData[1].products;
+              this.productsShiPInData = productManager.productData[2].products;
             this.productsJiuPinData = productManager.productData[3].products;
            
             }
@@ -101,15 +101,7 @@ func2();  // 报错  TypeError: number is not a function
                     this.showArrObj(this.productsDianZiData,0);
             	}
             };
-             base.showShiPin = function showShiPin()
-            {
-            	if (typeof(this.productsShiPInData)===undefined) {
-
-            	}else{
-                    this.tmplStrShiPin = '';
-                    this.showArrObj(this.productsShiPInData,1);
-            	}
-            };
+         
              base.showRiYongPin = function showRiYongPin()
             {
                 
@@ -118,8 +110,17 @@ func2();  // 报错  TypeError: number is not a function
             	}else{
             	 
                     this.tmplStrRiYongPin = '';
-                    this.showArrObj(this.productsRiYongPinData,2);
+                    this.showArrObj(this.productsRiYongPinData,1);
             	}
+            };
+                base.showShiPin = function showShiPin()
+            {
+              if (typeof(this.productsShiPInData)===undefined) {
+
+              }else{
+                    this.tmplStrShiPin = '';
+                    this.showArrObj(this.productsShiPInData,2);
+              }
             };
              base.showJiuLei = function showJiuLei()
             {
@@ -145,8 +146,10 @@ func2();  // 报错  TypeError: number is not a function
                     for(var index in objArr) 
                     { 
                         var dic = objArr[index];
-                        
-                        this.showDicObj(dic,index,currentListNum);
+                         
+                         var indexNums = index;
+
+                        this.showDicObj(dic,indexNums,currentListNum);
                     } 
                  if (currentListNum === 0) {
                  this.tmplStrDianZi = '<ul id = "dianzi"><p>电子类产品条目:</p>'+ this.tmplStrDianZi + '</ul>';
@@ -174,24 +177,39 @@ func2();  // 报错  TypeError: number is not a function
             // 
               
             if (typeof(objDic) === 'object') {
+
+              var indexNums = index;
+            
+
                for(var key in objDic)
                { 
+
               if (key === 'productName')
               {
-                if (currentListNum === 0) {
-                 this.tmplStrDianZi = this.tmplStrDianZi + '<li>' + objDic[key] + '<div><a>添加到购物车</a></div> <div><a>从购物车删除</a></div></li>';
+               var productValue=objDic['productValue'];
+               var productid = objDic['productID'];
+
+               var  productidadd = productid + '~' +'add'+'~'+currentListNum+'~'+'index'+indexNums;
+               var  productidremove = productid + '~' + 'remove'+'~'+currentListNum+'~'+'index'+indexNums;
+              
+               var aStr ='<div> <a id='+ "\'" + productidadd + "\'" + ' name="spanname">添加到购物车</a></div>' + '<div><span id='+ "\'"+productidremove +"\'" +'>从购物车删除</span></div>';
+      
+                if 
+                  (currentListNum === 0) {
+  
+                 this.tmplStrDianZi = this.tmplStrDianZi + '<li>' + 'name:' + objDic[key] + '      price:'+ productValue + aStr+'</li>';
                 }
 
               if (currentListNum === 1) {
 
-                this.tmplStrRiYongPin = this.tmplStrRiYongPin + '<li>' + objDic[key] + '<div><a>添加到购物车</a></div> <div><a>从购物车删除</a></div></li>';
+                this.tmplStrRiYongPin = this.tmplStrRiYongPin + '<li>'+ 'name:' + objDic[key] + '      price:'+ productValue + aStr +'</li>';
               }
               if (currentListNum === 2) {
 
-                this.tmplStrShiPin = this.tmplStrShiPin + '<li>' + objDic[key] + '<div><a>添加到购物车</a></div> <div><a>从购物车删除</a></div></li>';
+                this.tmplStrShiPin = this.tmplStrShiPin + '<li>' + 'name:' + objDic[key] + '      price:'+ productValue + aStr +'</li>';
               }
               if (currentListNum === 3) {
-                this.tmplStrJiuLei = this.tmplStrJiuLei + '<li>' + objDic[key] + '<div><a>添加到购物车</a></div> <div><a>从购物车删除</a></div></li>';
+                this.tmplStrJiuLei = this.tmplStrJiuLei + '<li>' + 'name:' + objDic[key] + '      price:'+ productValue + aStr +'</li>';
               }
         
               }
@@ -254,20 +272,308 @@ func2();  // 报错  TypeError: number is not a function
           
           document.write('<h1>阿拉丁的商场提供商品:</h1>');
 
+            // console.log(this.tmplStrDianZi);
+            // console.log(this.tmplStrRiYongPin);
+            // console.log(this.tmplStrShiPin);
+            // console.log(this.tmplStrJiuLei);
           document.write(this.tmplStrDianZi);
 
-          document.write(this.tmplStrShiPin);
+         
 
           document.write(this.tmplStrRiYongPin);  
+
+           document.write(this.tmplStrShiPin);
         
           document.write(this.tmplStrJiuLei);
+
+          document.write('<h2 id="look"> 查看我的购物车 </h2>');
 
 
         
       };
-       base.addClick = function addClick(argument) {
-           // body...
+     base.addElements = [];
+
+    base.fetchName = function fetchName (currentListNum,productid) {
+      // body... 
+    
+    if (currentListNum==0) {
+
+      for (var i = 0; i < this.productsDianZiData.length; i++) {
+         var product = this.productsDianZiData[i];
+      
+         if (product.productID == productid) {
+          return product.productName;
+         }
+      }
+    }
+
+    if (currentListNum==1) {
+
+      for (var i = 0; i < this.productsRiYongPinData.length; i++) {
+         var product = this.productsRiYongPinData[i];
+          
+         if (product.productID == productid) {
+          return product.productName;
+         }
+      }
+    }
+
+    if (currentListNum==2) {
+
+      for (var i = 0; i < this.productsShiPInData.length; i++) {
+         var product = this.productsShiPInData[i];
+        
+         if (product.productID == productid) {
+          return product.productName;
+         }
+      }
+    }
+
+    if (currentListNum==3) {
+
+      for (var i = 0; i < this.productsJiuPinData.length; i++) {
+         var product = this.productsJiuPinData[i];
+       
+         if (product.productID == productid) {
+          return product.productName;
+         }
+      }
+    }
+
+    };
+
+ base.fetchPrices = function fetchPrices (currentListNum,productid) {
+      // body... 
+    
+    if (currentListNum==0) {
+
+      for (var i = 0; i < this.productsDianZiData.length; i++) {
+         var product = this.productsDianZiData[i];
+       
+         if (product.productID == productid) {
+          return product.productValue;
+         }
+      }
+    }
+
+    if (currentListNum==1) {
+
+      for (var i = 0; i < this.productsRiYongPinData.length; i++) {
+         var product = this.productsRiYongPinData[i];
+         
+         if (product.productID == productid) {
+          return product.productValue;
+         }
+      }
+    }
+
+    if (currentListNum==2) {
+
+      for (var i = 0; i < this.productsShiPInData.length; i++) {
+         var product = this.productsShiPInData[i];
+         
+         if (product.productID == productid) {
+          return product.productValue;
+         }
+      }
+    }
+
+    if (currentListNum==3) {
+
+      for (var i = 0; i < this.productsJiuPinData.length; i++) {
+         var product = this.productsJiuPinData[i];
+        
+         if (product.productID == productid) {
+          return product.productValue;
+         }
+      }
+    }
+
+    };
+
+     base.clickEventAdd = function clickEventAdd (thisElement,event){  
+             console.log('增加');
+              var str = thisElement.id;
+              console.log(str);
+              var numlist =str.split('~')[2];
+              var productid =str.split('~')[0];
+               console.log(productid);
+           var nums = this.addElements.length;
+           if (typeof(thisElement.nums) === 'undefined') {
+            thisElement.nums = 0;
+           }
+            if (typeof(thisElement.names) === 'undefined') {
+            thisElement.names = this.fetchName(numlist,productid);
+           }
+              if (typeof(thisElement.prices) === 'undefined') {
+            thisElement.prices = this.fetchPrices(numlist,productid);
+           }
+
+            var numsClick = thisElement.nums;
+          
+           
+           if (this.addElements.length === 0) {
+                 thisElement.nums = 1;
+               if (nums != this.addElements.push(str)) {
+                   var suc =thisElement.names+'添加购物车成功'+ '  数量:'+ thisElement.nums;
+                 alert(suc);
+
+                 var strMes = '首次添加该产品' +thisElement.names +'  nums:'+ thisElement.nums;
+                  console.log(strMes);
+                }
+           }else
+           {
+            var lengths = this.addElements.length;
+             
+            var boolNewItem = false;
+            for (var i = 0; i <lengths ; i++) {
+
+            if ( this.addElements[i] == thisElement.id) {
+                boolNewItem = false;
+                   numsClick++;
+                   thisElement.nums = numsClick;
+                   var strMes = '购物车中已有该产品，'+ thisElement.names +' 累计量 nums:'+ numsClick;
+                 if (lengths != this.addElements.push( thisElement.id)) {
+               
+                   console.log(strMes);
+                var suc =thisElement.names+'添加购物车成功' + '  数量:'+ thisElement.nums;
+                 alert(suc);
+                  
+                }
+            }else  {
+              boolNewItem = true;
+            }
+           
+           }
+           if (boolNewItem) {
+              thisElement.nums = 1;
+               if (nums != this.addElements.push(str)) {
+                var suc =thisElement.names+'添加购物车成功'+ '  数量:'+ thisElement.nums;
+                 alert(suc);
+                 var strMes = '首次添加产品'+ thisElement.names +' 累计量 nums:'+ thisElement.nums;
+                  console.log(strMes);
+                }
+           }
+         
+           }
+          
        };
+
+       base.clickEventRemove = function clickEventRemove (thisElement,event){  
+               console.log('删除');
+             var str = thisElement.id;
+             var numlist =str.split('~')[2];
+              var productid =str.split('~')[0];
+           var nums = this.addElements.length;
+           if (typeof(thisElement.nums) === 'undefined') {
+            thisElement.nums = 0;
+           }
+            if (typeof(thisElement.names) === 'undefined') {
+            thisElement.names = this.fetchName(numlist,productid);
+           }
+            if ( this.addElements.length === 0) {
+               alert('当前购物车为空');
+            }else
+            {
+              var productID = thisElement.id.split('~')[0];
+              var lengths =this.addElements.length;
+              for (var i = 0; i < lengths; i++) {
+               var str = this.addElements[i];
+               
+               var strArrs = str.split('~');
+               if (strArrs[0] == productID) {
+                var index =this.addElements.indexOf(str);
+                console.log('删除前');
+                console.log(this.addElements);
+                console.log(productID);
+                console.log(str);
+                console.log(index);
+             var splitE = this.addElements.splice(index,1);
+              console.log( splitE);
+                 console.log('删除后');
+                console.log( this.addElements);
+                var suc= thisElement.names+'从购物车删除成功';
+                   alert(suc);
+               }else  {
+                 alert('购物车中没有该产品');
+               }
+              }
+              
+            }
+           
+            
+       };
+       base.addClick = function addClick(argument) {
+           
+      var spanElements =  document.getElementsByTagName('span');
+    
+       
+      for (var i = 0; i < spanElements.length; i++) {
+     
+         if (spanElements[i].nodeType === 1) {
+           
+          spanElements[i].addEventListener('click', function(e){
+           
+             base.clickEventRemove(this,e);
+          }, false);
+         }
+      }
+       var aElements =  document.getElementsByTagName("a");
+           // body...
+    
+
+     
+        for (var i = 0; i < aElements.length; i++) {
+           
+          if (aElements[i].nodeType === 1) {
+           
+            aElements[i].addEventListener('click', function(e){
+       
+            base.clickEventAdd(this,e);
+           
+            }, false);
+          }
+        }
+
+       var lookElement = document.getElementById('look');
+
+       lookElement.addEventListener('click', function(e){
+        
+        base.showAllGoods();
+
+       
+       }, false);
+
+       };
+
+       base.showAllGoods = function showAllGoods (argument) {
+         // body... 
+        
+        if (this.addElements.length === 0) {
+          alert('当前购物车为空');
+        }else
+        {
+           this.sums = 0;
+           var str = '';
+           for (var i = 0; i < this.addElements.length; i++) {
+              var elementID =  this.addElements[i];
+              var element = document.getElementById(elementID);
+              str = str +'<li>'+element.names+ '  price: '+element.prices+'   selectedNums:'+element.nums +'</li>'
+
+               this.sums = this.sums + element.prices*element.nums ;
+           }
+           str ='<ul>'+'您选择的产品清单：' + str + '</ul>';
+           document.write(str);
+           document.write('您总共消费:'+ this.sums.toFixed(2));
+        }
+         
+
+       
+
+       }
+       
+       base.sums = 0;
+
        base.removeClick = function removeClick(argument) {
            // body...
        };
@@ -278,20 +584,23 @@ func2();  // 报错  TypeError: number is not a function
            // body...
        };
 
-
+       
 
      return base;
    
 }();
 
-base.getProductsData();
+
 window.onload = function () {
     // body...
+    base.getProductsData();
     base.showDianZi();
     base.showRiYongPin();
     base.showShiPin(); 
     base.showJiuLei(); 
     base.readyHtmlElement();
+    base.addClick();
+
 };
 
 
